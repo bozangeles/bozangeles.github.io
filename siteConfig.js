52d520605394563df16531326a1618035e114dfa
Luke Van Seters <lukevanseters@gmail.com>
2020-09-24 22:06:45 -0600

    Starting point from London template

---
+++ siteConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: "Boz Angeles",
+  shortName: "Boz Angeles",
+  description: "Bozeman's Best and Brightest",
+  url: "https://gatsby-london.netlify.com", // Domain of your site without prefix!
+  prefix: "/",
+  author: "Bozeangeles",
+  twitter: "bozeangel.es"
+};
