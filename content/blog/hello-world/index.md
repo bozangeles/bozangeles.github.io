e8e2e32cda999d8a97cae34eeb5c6ef961448952
Luke Van Seters <lukevanseters@gmail.com>
2020-09-25 17:05:34 -0600

    Remove blog

--- content/blog/hello-world/index.md
+++
@@ -1,6 +0,0 @@
----
-title: Hop in
-date: "2015-05-01T22:12:03.284Z"
-thumbnail: ./peter-mccrae-hokenson-bC1_EE7eO38-unsplash.jpg
-description: good to see ya
----
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import { graphql, StaticQuery } from "gatsby";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import PostCard from "../components/postCard";
 
 // import "../utils/global.scss"
 import "../utils/normalize.css";
@@ -11,8 +10,6 @@ import "../utils/css/screen.css";
 //TODO: switch to staticQuery, get rid of comments, remove unnecessary components, export as draft template
 const BlogIndex = ({ data }, location) => {
   const siteTitle = data.site.siteMetadata.title;
-  const posts = data.allMarkdownRemark.edges;
-  let postCounter = 0;
 
   return (
     <Layout title={siteTitle}>
@@ -26,19 +23,6 @@ const BlogIndex = ({ data }, location) => {
           </header>
         )}
       </div>
-      <div className="post-feed">
-        {posts.map(({ node }) => {
-          postCounter++;
-          return (
-            <PostCard
-              key={node.fields.slug}
-              count={postCounter}
-              node={node}
-              postClass={`post`}
-            />
-          );
-        })}
-      </div>
     </Layout>
   );
 };
@@ -49,28 +33,6 @@ const indexQuery = graphql`
       siteMetadata {
         title
         description
-      }
-    }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      edges {
-        node {
-          excerpt
-          fields {
-            slug
-          }
-          frontmatter {
-            date(formatString: "MMMM DD, YYYY")
-            title
-            description
-            thumbnail {
-              childImageSharp {
-                fluid(maxWidth: 1360) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
-          }
-        }
       }
     }
   }
