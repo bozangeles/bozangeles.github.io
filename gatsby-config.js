3cbdfbe9bfc715c5f2e8bff42040e584f2a6f96c
Luke Van Seters <lukevanseters@gmail.com>
2020-09-25 17:28:26 -0600

    Whoops favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -101,7 +101,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `static/favicon.io`
+        icon: `static/favicon.ico`
       }
     },
     `gatsby-plugin-netlify`,
